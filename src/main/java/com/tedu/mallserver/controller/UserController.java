fc201265948284b6e3f6355321a4e8149d9ba471
qhapi <860137281@qq.com>
2022-07-03 16:01:55 +0800

    更新

---
+++ src/main/java/com/tedu/mallserver/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.tedu.mallserver.controller;
+
+import com.tedu.mallserver.pojo.ServerResult;
+import com.tedu.mallserver.pojo.UserDTO;
+import com.tedu.mallserver.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/user/login")
+    public ServerResult login(UserDTO userDTO){
+        Integer dao = userService.login(userDTO);
+        if(dao != -1) {
+            ServerResult serverResult = new ServerResult(0, "success", dao);
+
+            return serverResult;
+        }
+        else{
+            ServerResult serverResult = new ServerResult(-1, "failed", dao);
+
+            return serverResult;
+
+        }
+
+    }
+}
