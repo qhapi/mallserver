5671008898d80bd921b97f615a37fe249d334cb1
qhapi <860137281@qq.com>
2022-07-06 15:26:14 +0800

    删除了部分原项目模块

--- src/main/java/com/tedu/mallserver/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.tedu.mallserver.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.tedu.mallserver.pojo.UserDAO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<UserDAO> {
-}
