d442d03c5f746c8f366fe261bd2803eba9fc2f76
qhapi <860137281@qq.com>
2022-07-06 15:59:37 +0800

    删除全部模块，增加专业数据层

---
+++ src/main/java/com/tedu/mallserver/mapper/SpecialtyMapper.java
@@ -0,0 +1,9 @@
+package com.tedu.mallserver.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tedu.mallserver.pojo.SpecialtyDAO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SpecialtyMapper extends BaseMapper<SpecialtyDAO> {
+}
---
+++ src/main/java/com/tedu/mallserver/pojo/StudentUpdateDTO.java
@@ -0,0 +1,8 @@
+package com.tedu.mallserver.pojo;
+
+public class StudentUpdateDTO {
+    String username;
+    String spicture;
+    String phonenumber;
+
+}
